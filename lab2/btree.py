d55934ed5dda544ef2db053959cc4434fcf9415f
Artem-Litkovskyi <litkovskyi.artem@gmail.com>
2025-03-04 14:59:33 +0200

    B-Tree: important fixes. Add database simulation

--- lab2/btree.py
+++ lab2/btree.py
@@ -13,79 +13,78 @@ class BTreeNode:
 
 # key1 <= x < key2
 class BTree:
-    def __init__(self, key_provider=hash, order=2):
-        self.key_provider = key_provider
+    def __init__(self, key_hash_provider=hash, order=2):
+        self.key_hash_provider = key_hash_provider
         self.order = order
         self.root = BTreeNode(True, [], [])
 
     def __str__(self):
         return BTree._node_to_str(self.root)
 
-    def search(self, value):
-        return self._search_in_node(self.root, self.key_provider(value))
+    def search(self, key):
+        return self._search_in_node(self.root, self.key_hash_provider(key))
 
-    def search_lower(self, value):
+    def search_less(self, value):
         result = []
-        for values in self._search_in_node_lower(self.root, self.key_provider(value)):
+        for values in self._search_in_node_less(self.root, self.key_hash_provider(value)):
             result.extend(values)
         return result
 
     def search_greater(self, value):
         result = []
-        for values in self._search_in_node_greater(self.root, self.key_provider(value)):
+        for values in self._search_in_node_greater(self.root, self.key_hash_provider(value)):
             result.extend(values)
         return result
 
-    def _search_in_node(self, node: BTreeNode, key: int):
+    def _search_in_node(self, node: BTreeNode, key_hash: int):
+        if node.leaf:
+            for i, k in enumerate(node.keys):
+                if k != key_hash:
+                    continue
+                return node.children[i]
+            return []
+
+        child_index = self._get_child_index_by_key(node, key_hash)
+        return self._search_in_node(node.children[child_index], key_hash)
+
+    def _search_in_node_less(self, node: BTreeNode, key_hash: int):
         if node.leaf:
             results = []
             for i, k in enumerate(node.keys):
-                if k != key:
+                if k >= key_hash:
                     continue
-                results.append(node.children[i])
-            return results
-
-        child_index = self._get_child_index_by_key(node, key)
-        return self._search_in_node(node.children[child_index], key)
-
-    def _search_in_node_lower(self, node: BTreeNode, key: int):
-        if node.leaf:
-            results = []
-            for i, k in enumerate(node.keys):
-                if k >= key:
-                    continue
-                results.append(node.children[i])
+                results.extend(node.children[i])
             yield results
             return
 
-        child_indexes = self._get_child_indexes_by_key_lower(node, key)
+        child_indexes = self._get_child_indexes_by_key_less(node, key_hash)
         for i in child_indexes:
-            results = self._search_in_node_lower(node.children[i], key)
+            results = self._search_in_node_less(node.children[i], key_hash)
             yield from results
 
-    def _search_in_node_greater(self, node: BTreeNode, key: int):
+    def _search_in_node_greater(self, node: BTreeNode, key_hash: int):
         if node.leaf:
             results = []
             for i, k in enumerate(node.keys):
-                if k <= key:
+                if k <= key_hash:
                     continue
-                results.append(node.children[i])
+                results.extend(node.children[i])
             yield results
             return
 
-        child_indexes = self._get_child_indexes_by_key_greater(node, key)
+        child_indexes = self._get_child_indexes_by_key_greater(node, key_hash)
         for i in child_indexes:
-            results = self._search_in_node_greater(node.children[i], key)
+            results = self._search_in_node_greater(node.children[i], key_hash)
             yield from results
 
-    def insert(self, value):
-        new_things = self._insert_into_node(self.root, self.key_provider(value), value)
+    def insert(self, key, value):
+        new_things = self._insert_into_node(self.root, self.key_hash_provider(key), value)
 
-        # If child wasn't split, return
+        # If nothing was split, return
         if not new_things:
             return
 
-        # Otherwise insert a new tree level
+        # Otherwise insert a new root node
         middle_key, new_child = new_things
         self.root = BTreeNode(
             leaf=False,
@@ -93,17 +92,17 @@ class BTree:
             children=[self.root, new_child]
         )
 
-    def _insert_into_node(self, node: BTreeNode, key: int, value):
+    def _insert_into_node(self, node: BTreeNode, key_hash: int, value):
         if node.leaf:
-            self._insert_into_leaf(node, key, value)
+            self._insert_into_leaf(node, key_hash, value)
 
             if len(node.keys) <= self.order * 2:
                 return None
 
             return self._split_the_leaf(node)
         else:
-            child_index = self._get_child_index_by_key(node, key)
-            new_things = self._insert_into_node(node.children[child_index], key, value)
+            child_index = self._get_child_index_by_key(node, key_hash)
+            new_things = self._insert_into_node(node.children[child_index], key_hash, value)
 
             # If child wasn't split, return None
             if not new_things:
@@ -119,15 +118,18 @@ class BTree:
 
             return self._split_the_node(node)
 
-    def _insert_into_leaf(self, leaf: BTreeNode, key: int, value):
+    def _insert_into_leaf(self, leaf: BTreeNode, key_hash: int, value):
         for i, k in enumerate(leaf.keys):
-            if k <= key:
+            if k < key_hash:
                 continue
-            leaf.keys.insert(i, key)
-            leaf.children.insert(i, value)
+            elif k == key_hash:
+                leaf.children[i].append(value)
+                return
+            leaf.keys.insert(i, key_hash)
+            leaf.children.insert(i, [value])
             return
-        leaf.keys.append(key)
-        leaf.children.append(value)
+        leaf.keys.append(key_hash)
+        leaf.children.append([value])
 
     def _split_the_leaf(self, node: BTreeNode):
         right_half = BTreeNode(
@@ -140,29 +142,29 @@ class BTree:
         return right_half.keys[0], right_half
 
     def _split_the_node(self, node: BTreeNode):
-        middle_key = node.keys[0]
+        middle_key = node.keys[self.order]
         right_half = BTreeNode(
-            leaf=True,
+            leaf=False,
             keys=node.keys[self.order+1:],
             children=node.children[self.order+1:]
         )
         node.keys = node.keys[:self.order]
-        node.children = node.children[:self.order]
+        node.children = node.children[:self.order+1]
         return middle_key, right_half
 
     @staticmethod
-    def _get_child_index_by_key(node: BTreeNode, key: int):
+    def _get_child_index_by_key(node: BTreeNode, key_hash: int):
         for i, k in enumerate(node.keys):
-            if k <= key:
+            if k <= key_hash:
                 continue
             return i
         return len(node.keys)
 
     @staticmethod
-    def _get_child_indexes_by_key_lower(node: BTreeNode, key: int):
+    def _get_child_indexes_by_key_less(node: BTreeNode, key_hash: int):
         indexes = []
         for i, k in enumerate(node.keys):
-            if k <= key:
+            if k <= key_hash:
                 indexes.append(i)
                 continue
             indexes.append(i)
@@ -170,10 +172,10 @@ class BTree:
         return indexes
 
     @staticmethod
-    def _get_child_indexes_by_key_greater(node: BTreeNode, key: int):
+    def _get_child_indexes_by_key_greater(node: BTreeNode, key_hash: int):
         indexes = []
         for i, k in enumerate(node.keys):
-            if k <= key:
+            if k <= key_hash:
                 continue
             indexes.append(i)
         indexes.append(len(node.keys))
--- lab2/btree_test.py
+++ lab2/btree_test.py
@@ -1,4 +1,5 @@
 import unittest
+
 from btree import *
 
 
@@ -13,17 +14,17 @@ TEST_NODES = BTreeNode(
                 BTreeNode(
                     True,
                     [2, 3],
-                    ['2', '3']
+                    [['2'], ['3']]
                 ),
                 BTreeNode(
                     True,
                     [5, 7, 8],
-                    ['5', '7', '8']
+                    [['5'], ['7'], ['8']]
                 ),
                 BTreeNode(
                     True,
                     [14, 16],
-                    ['14', '16']
+                    [['14'], ['16']]
                 )
             ]
         ),
@@ -34,17 +35,17 @@ TEST_NODES = BTreeNode(
                 BTreeNode(
                     True,
                     [19, 20, 22],
-                    ['19', '20', '22']
+                    [['19'], ['20'], ['22']]
                 ),
                 BTreeNode(
                     True,
-                    [24, 27, 27, 29],
-                    ['24', '27', '2 7', '29']
+                    [24, 27, 29],
+                    [['24'], ['27', '2 7'], ['29']]
                 ),
                 BTreeNode(
                     True,
                     [33, 34, 38, 39],
-                    ['33', '34', '38', '39']
+                    [['33'], ['34'], ['38'], ['39']]
                 )
             ]
         )
@@ -52,48 +53,57 @@ TEST_NODES = BTreeNode(
 )
 
 
-def test_key_provider(value: str):
+def get_key_for_value(value: str):
     return int(value.replace(' ', ''))
+
+
+def test_key_hash_provider(key: int):
+    return key
 
 
 class SearchTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.tree = BTree(test_key_provider, 2)
+        cls.tree = BTree(test_key_hash_provider, 2)
         cls.tree.root = TEST_NODES
+
     def test_existing(self):
         value = '29'
-        self.assertEqual([value], SearchTest.tree.search(value))
+        key = get_key_for_value(value)
+        self.assertIn(value, SearchTest.tree.search(key))
 
     def test_common_key(self):
         values = ['27', '2 7']
         for value in values:
-            self.assertIn(value, SearchTest.tree.search(value))
+            key = get_key_for_value(value)
+            self.assertIn(value, SearchTest.tree.search(key))
 
     def test_not_existing(self):
-        self.assertEqual([], SearchTest.tree.search('28'))
+        value = '28'
+        key = get_key_for_value(value)
+        self.assertEqual(0, len(SearchTest.tree.search(key)))
 
-    def test_search_lower(self):
+    def test_search_less(self):
         expected_values = ['2', '3', '5']
-        actual_values = SearchTest.tree.search_lower('7')
+        actual_values = SearchTest.tree.search_less(get_key_for_value('7'))
         for v in expected_values:
             self.assertIn(v, actual_values)
 
-    def test_search_lower_not_existing(self):
+    def test_search_less_not_existing(self):
         expected_values = ['2', '3', '5']
-        actual_values = SearchTest.tree.search_lower('6')
+        actual_values = SearchTest.tree.search_less(get_key_for_value('6'))
         for v in expected_values:
             self.assertIn(v, actual_values)
 
     def test_search_greater(self):
         expected_values = ['24', '27', '2 7', '29', '33', '34', '38', '39']
-        actual_values = SearchTest.tree.search_greater('22')
+        actual_values = SearchTest.tree.search_greater(get_key_for_value('22'))
         for v in expected_values:
             self.assertIn(v, actual_values)
 
     def test_search_greater_not_existing(self):
         expected_values = ['24', '27', '2 7', '29', '33', '34', '38', '39']
-        actual_values = SearchTest.tree.search_greater('23')
+        actual_values = SearchTest.tree.search_greater(get_key_for_value('23'))
         for v in expected_values:
             self.assertIn(v, actual_values)
 
@@ -101,28 +111,32 @@ class SearchTest(unittest.TestCase):
 class InsertionTest(unittest.TestCase):
     @classmethod
     def setUp(cls):
-        cls.tree = BTree(test_key_provider, 2)
+        cls.tree = BTree(test_key_hash_provider, 2)
 
-    def insertion(self, values):
+    def assert_insertion(self, values):
         for value in values:
-            InsertionTest.tree.insert(value)
-            self.assertIn(value, InsertionTest.tree.search(value))
+            key = get_key_for_value(value)
+            InsertionTest.tree.insert(key, value)
+            self.assertIn(value, InsertionTest.tree.search(key))
 
     def test_insert(self):
         with self.subTest('Insert into the root node'):
-            self.insertion(('59','23','7', '97'))
+            self.assert_insertion(('59','23','7', '97'))
 
-        with self.subTest('Insert that causes the root node to split'):
-            self.insertion(('73',))
+        with self.subTest('Insert that causes the root (leaf) to split, creating a new root node'):
+            self.assert_insertion(('73',))
 
-        with self.subTest('Insert into left and right subtrees'):
-            self.insertion(('67', '19'))
+        with self.subTest('Insert into the left and right subtrees'):
+            self.assert_insertion(('67', '19'))
 
-        with self.subTest('Insert that causes the right subtree to split'):
-            self.insertion(('79',))
+        with self.subTest('Insert that causes the right leaf to split, adding a new key to the root'):
+            self.assert_insertion(('79',))
 
         with self.subTest('Insert another value with a key that is already in the tree'):
-            self.insertion(('5 9',))
+            self.assert_insertion(('5 9',))
+
+        with self.subTest('Inserts that cause the root (not leaf) to split, creating a new root node'):
+            self.assert_insertion(('100', '101', '102', '103', '104', '105'))
 
 
 if __name__ == '__main__':
